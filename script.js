1f6b1c65163c1bbb02a444e0235d8c36869db5b7
Christian Fielding <Christian.Fielding@my.utsa.edu>
2026-06-01 14:18:00 -0500

    Week 1 goal

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log("Hello World");
+const addBtn = document.getElementById("addBtn");
+const taskInput = document.getElementById("taskInput");
+const tastList = document.getElementById("taskList");
+
+addBtn.addEventListener("click", function (){
+
+    const taskText = taskInput.value;
+
+    const li = document.createElement("li");
+
+    li.textContent = tastText;
+
+    const deleteBtn = document.createElement("button");
+
+    deleteBtn.textContent = "Delete";
+
+    deleteBtn.addEventListener("click", function () {
+        li.remove();
+    });
+
+    li.appendChild(deleteBtn);
+
+    taskList.appendChild(li);
+
+    taskInput.value = "";
+});
